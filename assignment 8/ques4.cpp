c6a3a305685c19513b741de6cd96c4c3ac5b52aa
trishaaroraa <trishaarora1331@gmail.com>
2025-11-30 01:17:13 +0530

    Create ques4.cpp

---
+++ assignment 8/ques4.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* createNode(int value) {
+    Node* n = new Node();
+    n->data = value;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+bool isBSTUtil(Node* root, int minVal, int maxVal) {
+    if(root == NULL)
+        return true;
+
+    if(root->data <= minVal || root->data >= maxVal)
+        return false;
+
+    return isBSTUtil(root->left, minVal, root->data) &&
+           isBSTUtil(root->right, root->data, maxVal);
+}
+
+bool isBST(Node* root) {
+    return isBSTUtil(root, INT_MIN, INT_MAX);
+}
+
+int main() {
+    Node* root = createNode(50);
+    root->left = createNode(30);
+    root->right = createNode(70);
+    root->left->left = createNode(20);
+    root->left->right = createNode(40);
+    root->right->left = createNode(60);
+    root->right->right = createNode(80);
+
+    if(isBST(root))
+        cout << "Tree is a BST";
+    else
+        cout << "Tree is not a BST";
+
+    return 0;
+}
